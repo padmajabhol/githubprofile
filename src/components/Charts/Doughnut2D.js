e8d66ba4bcc4d57cb2622f095db1edc8f1817156
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2022-01-31 16:15:13 +0530

    integrate fusioncharts

--- src/components/Charts/Doughnut2D.js
+++ src/components/Charts/Doughnut2D.js
@@ -1,8 +1,49 @@
+/* eslint-disable object-shorthand */
 /* eslint-disable arrow-body-style */
+// STEP 1 - Include Dependencies
+// Include react
 import React from 'react';
 
-const Doughnut2D = () => {
-  return <div>Doughnut2D</div>;
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts';
+
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Include the chart type
+import Chart from 'fusioncharts/fusioncharts.charts';
+
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy';
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+// STEP 2 - Chart Data
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: 'doughnut2d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: 'Stars per language',
+        theme: 'candy',
+        decimals: 0,
+        doughnutRadius: '55%',
+        paletteColors: data.color,
+        showPercentValues: 0,
+      },
+      // Chart Data
+      data: data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Doughnut2D;
+export default ChartComponent;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,13 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-shadow */
+/* eslint-disable camelcase */
 /* eslint-disable operator-assignment */
 /* eslint-disable no-param-reassign */
 /* eslint-disable dot-notation */
 /* eslint-disable spaced-comment */
 /* eslint-disable no-unused-vars */
 /* eslint-disable arrow-body-style */
+import { items } from 'fusioncharts';
 import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../Context/context';
@@ -11,48 +15,119 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  let languages = repos.reduce((total, item) => {
-    const { language } = item; //destructuring language out of item
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item; //destructuring language out of item
     if (!language) return total; //just return total
     if (!total[language]) {
       //if its not on the object create a new value
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       //if its already on the object add the value
-      total[language] = { ...total[language], value: total[language].value + 1 };
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
     }
     // console.log(language);
     // console.log((total[language] = 30));
     return total;
   }, {});
-  languages = Object.values(languages)
+  //turn languages obj to an array
+  const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
     })
     .slice(0, 5);
-  const chartData = [
-    {
-      label: 'Venezuela',
-      value: '290',
-      color: '#7A0BC0',
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
     },
     {
-      label: 'Saudi',
-      value: '260',
-      color: '#FA58B6',
+      stars: {},
+      forks: {},
     },
-    {
-      label: 'Canada',
-      value: '180',
-      color: '#270082',
-    },
-  ];
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+  // const chartData = [
+  //   {
+  //     label: languages[0].label,
+
+  //     value: languages[0].value,
+  //     color: '#FFC600',
+  //   },
+  //   {
+  //     label: languages[1].label,
+
+  //     value: languages[1].value,
+  //     color: '#E900FF',
+  //   },
+  //   {
+  //     label: languages[2].label,
+
+  //     value: languages[2].value,
+  //     color: '#5800FF',
+  //   },
+  //   {
+  //     label: languages[3].label,
+
+  //     value: languages[3].value,
+  //     color: '#CFFFDC',
+  //   },
+  //   {
+  //     label: languages[4].label,
+
+  //     value: languages[4].value,
+  //     color: '#1F1D36',
+  //   },
+  // ];
   return (
-    <div>
-      {/* <ExampleChart data={chartData} /> */}
-      <Pie3D data={languages} />
-    </div>
+    <section className='section'>
+      <Wrapper className='section-name'>
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
+      </Wrapper>
+    </section>
   );
 };
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`;
 
 export default Repos;
